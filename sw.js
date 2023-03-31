244ccf6a580b3be1f1d60b0c3f76b61a412fb904
Audrico <drcsm2013@gmail.com>
2023-03-31 08:24:49 +0700

    balek

--- sw.js
+++ sw.js
@@ -56,8 +56,3 @@ const cacheFiles = [
     );
   });
   
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('./sw.js');
-    });
-}
