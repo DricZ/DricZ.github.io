935f7767d34f418ae38cb471075369d983214e1d
Audrico <drcsm2013@gmail.com>
2023-03-31 08:23:17 +0700

    up

--- sw.js
+++ sw.js
@@ -56,3 +56,8 @@ const cacheFiles = [
     );
   });
   
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('./sw.js');
+    });
+}
